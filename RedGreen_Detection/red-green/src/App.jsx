83ae0588db1902c6a47e1c7a592e38f0c486c913
Shanuka2003-01 <153509872+Shanuka2003-01@users.noreply.github.com>
2024-03-13 19:57:25 +0530

    Red green color blindness detection

---
+++ RedGreen_Detection/red-green/src/App.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function Questionnaire() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [usedImages, setUsedImages] = useState([]);
+  const [score, setScore] = useState(0);
+
+  const images = [
+    { src: './images/1.jpg', numbers: [2, 1] },
+    { src: './images/3.jpg', numbers: [0, 4] },
+    { src: './images/4.jpg', numbers: [4, 6] },
+    { src: './images/7.jpg', numbers: [9, 2] },
+    { src: './images/8.jpg', numbers: [5, 4] },
+    { src: './images/9.jpg', numbers: [9, 6] },
+    { src: './images/10.jpg', numbers: [0, 2] },
+    { src: './images/11.jpg', numbers: [6, 4] },
+    { src: './images/15.jpg', numbers: [5, 6] },
+    { src: './images/21.jpg', numbers: [6, 2] },
+    { src: './images/27.jpg', numbers: [4, 4] },
+    { src: './images/31.jpg', numbers: [1, 6] },
+    { src: './images/35.jpg', numbers: [2, 2] },
+    { src: './images/36.jpg', numbers: [7, 4] },
+    { src: './images/41.jpg', numbers: [2, 6] },
+    { src: './images/46.jpg', numbers: [1, 2] },
+    { src: './images/61.jpg', numbers: [8, 4] },
+    { src: './images/84.jpg', numbers: [8, 6] },
+    { src: './images/87.jpg', numbers: [3, 2] },
+    { src: './images/90.jpg', numbers: [3, 4] },
+  ];
+
+  useEffect(() => {
+    if (usedImages.length === images.length) {
+      // Resetting used images when all images have been used
+      setUsedImages([]);
+    }
+  }, [currentQuestionIndex, usedImages, images.length]);
+
+  const getRandomImage = () => {
+    const remainingImages = images.filter(image => !usedImages.includes(image));
+    return remainingImages[Math.floor(Math.random() * remainingImages.length)];
+  };
+
+  const handleAnswerSubmission = () => {
+    const correctNumbers = currentImage.numbers;
+    const userEnteredNumber = parseInt(userAnswer);
+
+    if (correctNumbers.includes(userEnteredNumber)) {
+      setIsCorrect(true);
+      setScore(score + 10);
+    } else {
+      setIsCorrect(false);
+    }
+  };
+
+  const nextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setUserAnswer('');
+    setIsCorrect(null);
+    const currentImage = getRandomImage(); // Accesing the new image here
+    setUsedImages([...usedImages, currentImage]);
+  };
+
+  const currentImage = currentQuestionIndex < images.length ? images[currentQuestionIndex] : null;
+
+  return (
+    <>
+    <h1>Ishihara Test</h1>
+    <div className='test'>
+      {currentImage ? (
+        <div className='imageDisplay'>
+          <img src={currentImage.src} alt="Number" />
+          <br />
+          <label name="answer"> Enter the number displayed in the image : </label>
+          <br />
+          <input
+            type="text"
+            name="answer"
+            value={userAnswer}
+            onChange={(e) => setUserAnswer(e.target.value)}
+            required
+          />
+          <br />
+          <button onClick={handleAnswerSubmission}>Submit Answer</button>
+          {isCorrect === true && <p>Correct Answer!</p>}
+          {isCorrect === false && <p>Incorrect Answer!</p>}
+          {isCorrect !== null && (
+            <button onClick={nextQuestion}>Next Question</button>
+          )}
+        </div>
+      ) : (
+        <p>Congratulations! You have completed the questionnaire.</p>
+      )}
+    </div>
+    </>
+  );
+}
+
+export default Questionnaire;
