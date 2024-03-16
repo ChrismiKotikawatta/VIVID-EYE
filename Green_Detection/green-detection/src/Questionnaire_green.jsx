ce5473fcfa3f7eb570a9040f95a159f5ea36d633
Shanuka2003-01 <153509872+Shanuka2003-01@users.noreply.github.com>
2024-03-16 17:03:56 +0530

    Adding a score

--- Green_Detection/green-detection/src/Questionnaire_green.jsx
+++ Green_Detection/green-detection/src/Questionnaire_green.jsx
@@ -5,8 +5,11 @@ import './App.css';
 function Questionnaire_green(){
     // Initalizing a state variable 'usedImages' and function 'setUsedImages' that keeps track on images that used.
     const[usedImages, setUsedImages] = useState([]);
-
-    
+    const[question, setQuestion] = useState(0);
+    // chwcks if user entered the correct answer
+    const [isCorrect, setIsCorrect] = useState(null);
+    // tracks the users score
+    const [score, setScore] = useState(0);
 
     const greenImages = [
         {src: './images/0.jpg', answer: [9, 7]},
@@ -26,13 +29,32 @@ function Questionnaire_green(){
         if(usedImages.length === greenImages.length){
             setUsedImages([]);
         }
-    },[usedImages, greenImages.length]);
+    },[question,usedImages, greenImages.length]);
 
     // displays the images randomly which is not used in useImages array.
     const getRandomImage = () => {
         const remainingImages = greenImages.filter(image => !usedImages.includes(image));
         return remainingImages[Math.floor(Math.random() * remainingImages.length)];
     };    
+
+    // function that is used to handle users answer submission
+    const handleSubmission = () =>{
+        const correctAnswer = currentImage.answer();
+        const userAnswer = parseInt(question);
+
+        if(correctAnswer.includes(userAnswer)){
+            setIsCorrect(true);
+            if(userAnswer === correctAnswer[1]){
+                setScore(score + 5);
+            }
+            else if(userAnswer === correctAnswer[0]){
+                setScore(score + 10);
+            }
+            else{
+                setIsCorrect(false);
+            }
+        }
+    };
 
 }
 export default Questionnaire_green;
