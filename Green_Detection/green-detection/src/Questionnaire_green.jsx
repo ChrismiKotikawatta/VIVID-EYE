bfad33ba7c4916a77410f1474c0b8fd762e21cbb
Shanuka2003-01 <153509872+Shanuka2003-01@users.noreply.github.com>
2024-03-16 16:33:02 +0530

    Randomizing color plates

--- Green_Detection/green-detection/src/Questionnaire_green.jsx
+++ Green_Detection/green-detection/src/Questionnaire_green.jsx
@@ -3,8 +3,9 @@ import './App.css';
 
 
 function Questionnaire_green(){
-
+    // Initalizing a state variable 'usedImages' and function 'setUsedImages' that keeps track on images that used.
     const[usedImages, setUsedImages] = useState([]);
+
     
 
     const greenImages = [
@@ -20,12 +21,18 @@ function Questionnaire_green(){
         {src: './images/0.jpg', answer: [9, 7]},
     ]
 
+    // rests the usedImages array when the length of usedImages array is equal to greenImages's length
     useEffect(() =>{
         if(usedImages.length === greenImages.length){
-            //Resetting the used images
             setUsedImages([]);
         }
     },[usedImages, greenImages.length]);
+
+    // displays the images randomly which is not used in useImages array.
+    const getRandomImage = () => {
+        const remainingImages = greenImages.filter(image => !usedImages.includes(image));
+        return remainingImages[Math.floor(Math.random() * remainingImages.length)];
+    };    
 
 }
 export default Questionnaire_green;
