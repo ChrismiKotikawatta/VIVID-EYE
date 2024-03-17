9f0edf305d30faf82311c4748ddb59f9adeb7058
Shanuka2003-01 <153509872+Shanuka2003-01@users.noreply.github.com>
2024-03-17 11:55:23 +0530

    Desigining the UI

--- Green_Detection/green-detection/src/App.jsx
+++ Green_Detection/green-detection/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import Questionnaire_green from './Questionnaire_green'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
       <Questionnaire_green/>
--- Green_Detection/green-detection/src/Questionnaire_green.jsx
+++ Green_Detection/green-detection/src/Questionnaire_green.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './Questionnaire_green.css';
-import './App.css';
 
 
 function Questionnaire_green(){
